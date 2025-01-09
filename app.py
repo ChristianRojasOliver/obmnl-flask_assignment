83294beeb291651c728d39dab328d8dd64c02f11
ChristianRojasOliver <christian.rojas201@gmail.com>
2025-01-09 13:51:01 -0500

    initial set up

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 # Import libraries
+import flask from Flask, request, url_for, redirect, render_template
 
 # Instantiate Flask functionality
+app = Flask("Financial Transaction Recording System")
 
 # Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 
 # Read operation
 
